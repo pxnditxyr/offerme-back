c1855f78311f0190103121f1c9fe47a73c08dfa3
Yuki xD <pxnditxyr@gmail.com>
2023-10-20 19:49:12 -0400

    feat: 🚀🌟 added new Roles in ValidRoles enum 👮🤵⛹️

--- src/auth/enums/valid-roles.enum.ts
+++ src/auth/enums/valid-roles.enum.ts
@@ -2,7 +2,9 @@ import { registerEnumType } from '@nestjs/graphql'
 
 export enum ValidRoles {
   ADMIN = 'ADMIN',
-  USER = 'USER'
+  USER = 'USER',
+  SELLER = 'SELLER',
+  COMPANY_REPRESENTATIVE = 'COMPANY_REPRESENTATIVE',
 }
 
 registerEnumType( ValidRoles, { name: 'ValidRoles' } )
